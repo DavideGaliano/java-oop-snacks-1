a1e34e6b2f16f19e200f51b67ff399b9b2877899
DavideGaliano <jconstantin@tiscali.it>
2024-06-29 10:15:47 +0200

    update metodo

--- src/ListOrder.java
+++ src/ListOrder.java
@@ -4,16 +4,20 @@ import java.util.Collections;
 public class ListOrder {
 
     public static void main(String[] args) {
-        // Step 1: Creare un ArrayList di 5 nomi non ordinati
+        // Step 1: Creare un ArrayList di 10 nomi non ordinati
         ArrayList<String> names = new ArrayList<>();
         names.add("Marco");
         names.add("Luca");
         names.add("Anna");
-        names.add("Davide");
+        names.add("Elena");
         names.add("Francesco");
-       
+        names.add("Paolo");
+        names.add("Sara");
+        names.add("Giulia");
+        names.add("Alessandro");
+        names.add("Martina");
 
-        // bubblesort
+        //bubblesort
         bubbleSort(names);
         System.out.println("Array ordinato manualmente: " + names);
 
@@ -25,15 +29,19 @@ public class ListOrder {
     // Metodo per ordinare l'ArrayList usando Bubble Sort
     public static void bubbleSort(ArrayList<String> list) {
         int n = list.size();
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = 0; j < n - 1 - i; j++) {
-                if (list.get(j).compareTo(list.get(j + 1)) > 0) {
+        boolean swapped;
+        do {
+            swapped = false;
+            for (int i = 0; i < n - 1; i++) {
+                if (list.get(i).compareTo(list.get(i + 1)) > 0) {
                     // Scambia gli elementi
-                    String temp = list.get(j);
-                    list.set(j, list.get(j + 1));
-                    list.set(j + 1, temp);
+                    String temp = list.get(i);
+                    list.set(i, list.get(i + 1));
+                    list.set(i + 1, temp);
+                    swapped = true;
                 }
             }
-        }
+            n--; // Riduce il range di confronto perché l'ultimo elemento è già ordinato
+        } while (swapped);
     }
 }
